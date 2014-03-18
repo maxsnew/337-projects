c11a57587f886a4c55c17853f5b5936768270b92
Max New <maxsnew@gmail.com>
2014-03-17 23:30:47 -0500

    Start making the db. Food Groups first

---
+++ recipes/db/schema.sql
@@ -0,0 +1,40 @@
+-- CREATE TABLE foods (
+       -- food_name Text primary key,
+       -- food_group reference
+       -- calories      Real,
+       -- protein       Real,
+       -- fat           Real,
+       -- carbohydrates Real,
+       -- sugars        Real
+       -- )
+
+-- CREATE TABLE FoodGroups (
+--        GroupName TEXT,
+--        GroupId   INTEGER
+-- );
+
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0100, 'Dairy and Egg Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0200, 'Spices and Herbs');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0300, 'Baby Foods');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0400, 'Fats and Oils');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0500, 'Poultry Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0600, 'Soups, Sauces, and Gravies');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0700, 'Sausages and Luncheon Meats');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0800, 'Breakfast Cereals');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (0900, 'Fruits and Fruit Juices');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1000, 'Pork Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1100, 'Vegetables and Vegetable Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1200, 'Nut and Seed Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1300, 'Beef Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1400, 'Beverages');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1500, 'Finfish and Shellfish Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1600, 'Legumes and Legume Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1700, 'Lamb, Veal, and Game Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1800, 'Baked Products');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (1900, 'Sweets');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (2000, 'Cereal Grains and Pasta');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (2100, 'Fast Foods');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (2200, 'Meals, Entrees, and Side Dishes');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (2500, 'Snacks');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (3500, 'American Indian/Alaska Native Foods');
+INSERT INTO FoodGroups(GroupId, GroupName) VALUES (3600, 'Restaurant Foods');
