6750ed37e7818593c657020a3744e1916b69c4cc
Max New <maxsnew@gmail.com>
2014-03-17 20:19:54 -0500

    Implement ingredient amount parsing

--- recipes/download.py
+++ recipes/download.py
@@ -5,28 +5,38 @@ def download_recipe(url):
     """Downloads a recipe from an allrecipes.com url and extracts the
        relevant part of the page""" 
 
-    name       = extract_recipe_name(url)
-    better_url = 'http://allrecipes.com/recipe/{0}/kitchenview.aspx'.format(name)
-    r          = requests.get(better_url)
+    r          = requests.get(url)
     html       = r.text
     return extract_recipe(html)
 
 def extract_recipe(html):
-    """Extract the recipe from the kitchen view of an allrecipes recipe"""
+    """Extract the recipe from the kitchen view of an allrecipes recipe,
+       returns a pair of lists, ingredients and directions"""
     soup = BeautifulSoup(html)
-    directions = [
-        div.get_text()
-        for div in soup.find(id='directions-wrapper')
-                       .findAll('div', {'class': 'direction'})
-    ]
-    ingreds = [
-        div.get_text()
-        for div in soup.find('div', {'class', 'ingred-div'}).findAll('li')
-    ]
+    directions = []
+    # directions = [
+    #     div.get_text()
+    #     for div in soup.find(id='directions-wrapper')
+    #                    .findAll('div', {'class': 'direction'})
+    # ]
+    ingreds = extract_ingredients(soup)
     return (ingreds, directions)
+
+def extract_ingredients(soup):
+    ingreds = [
+        make_ingredient(li)
+        for ul in soup.findAll('ul', {'class', 'ingredient-wrap'})
+        for li in ul.findAll('li')
+    ]
+    return ingreds
     
-def extract_recipe_name(url):
-    """From an allrecipes.com recipe url, extract the name of the recipe"""
-    pieces   = url.split('/')
-    recipe_i = pieces.index('recipe')
-    return pieces[recipe_i+1]
+def make_ingredient(li):
+    amount_span = li.find('span', { 'class': 'ingredient-amount'})
+    amount = None
+    if amount_span is not None:
+        amount = amount_span.get_text() 
+    name = li.find('span', { 'class': 'ingredient-name'}).get_text()
+    return {
+        'amount': amount,
+        'name'  : name
+    }
